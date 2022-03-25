90f94aa0c10d2dda48e7a0c16dae42aabf2e2713
shashank hebbar <86871679+Shashank850458@users.noreply.github.com>
2022-03-25 21:42:44 +0530

    Update question no1.js

--- question no1.js
+++ question no1.js
@@ -1,5 +1,5 @@
-https://github.com/rvsp/typescript-oops/blob/master/Practice/Movie.md
---------------------
+// 1.https://github.com/rvsp/typescript-oops/blob/master/Practice/Movie.md
+//--------------------
 class Movie{
   constructor(title,studio,rating="PG"){
     this.title = title;
